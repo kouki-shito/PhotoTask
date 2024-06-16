79033b3fb937f0e14f697712a4ee9eba91d50f2b
kouki-shito <likegameskou@gmail.com>
2024-06-16 19:45:01 +0900

    Initial Commit

---
+++ PhotoTask/PhotoTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoTaskApp.swift
+//  PhotoTask
+//
+//  Created by 市東 on 2024/06/16.
+//
+
+import SwiftUI
+
+@main
+struct PhotoTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
